56d42b85ad08fee259c9b25f58ac470353313f15
QuackieMackie <dev.quackiemackie@gmail.com>
2024-10-28 14:08:27 +0000

    Add Sadness resources and items
    Introduced textures, models, and blocks for new "Sadness" themed materials including raw sadness, sadness ingot, sadness block, and sadness ore. Updated main mod classes to register these new items and blocks, and moved language file to reflect changes.

---
+++ src/main/java/net/quackiemackie/pathoscraft/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.quackiemackie.pathoscraft.block;
+
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.quackiemackie.pathoscraft.PathosCraft;
+import net.quackiemackie.pathoscraft.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PathosCraft.MOD_ID);
+
+    public static final DeferredBlock<Block> SADNESS_BLOCK = registerBlock("sadness_block", () ->
+            new Block(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    public static final DeferredBlock<Block> SADNESS_ORE = registerBlock("sadness_ore", () ->
+            new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(3.0f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new BlockItem.Properties()));
+    }
+
+    public static void register(IEventBus modEventBus){
+        BLOCKS.register(modEventBus);
+    }
+}
---
+++ src/main/java/net/quackiemackie/pathoscraft/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.quackiemackie.pathoscraft.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.quackiemackie.pathoscraft.PathosCraft;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PathosCraft.MOD_ID);
+
+    //Ingredients
+    public static final DeferredItem<Item> RAW_SADNESS = ITEMS.register("raw_sadness", () ->
+            new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SADNESS_INGOT = ITEMS.register("sadness_ingot", () ->
+            new Item(new Item.Properties()));
+
+    public static void register(IEventBus modEventBus){
+        ITEMS.register(modEventBus);
+    }
+}
